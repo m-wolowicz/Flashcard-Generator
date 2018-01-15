2a66f8c2da2ce96e2f69d25bd718e81c374df0ad
Melissa Wolowicz <melissawolowicz@MW-iMac.local>
2018-01-15 17:48:27 -0500

    Added main.js file

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,18 +1,24 @@
 //Cloze-Deleted flashcards, which present partial text "... was the first president of the United States.", and the full text when the user requests it "George Washington was the first president of the United States."
 
-//Define a Node module that exports a constructor for creating cloze-deletion flashcards:
-module.exports = ClozeCard;
-
 //Constructor for the cloze-deletion flashcards
 // The constructor should accept two arguments: "text" and "cloze".
-var ClozeCard = function(text, cloze) {
-	this.text = text;
-	//Cloze property that contains only the cloze-deleted portion of the text.
-	this.cloze = cloze;
-	//The constructed object should have a partial property that contains only the partial text.
+function ClozeCard(text, cloze) {
+	//Creating a scope-safe constructor
+	if(this instanceof ClozeCard) {
+		//The .split() method removes the cloze from text
+		this.text = text;
+		//Cloze property that contains only the cloze-deleted portion of the text.
+		this.cloze = cloze;
+		//partial text:
+		this.partial = this.text.replace(this.cloze, "...");
+		//fullText:
+		this.fullText = text;
 
-	//The constructed object should have a fullText property that contains only the full text.
+	} else {
+		//This makes the constructor so that users can call them with or without the new keyword.
+		return new ClozeCard(text, cloze);
+	}
+}
 
-	//The constructor should throw or log an error when the cloze deletion does not appear in the input text.
-
-};
+//Define a Node module that exports a constructor for creating cloze-deletion flashcards:
+module.exports = ClozeCard;
